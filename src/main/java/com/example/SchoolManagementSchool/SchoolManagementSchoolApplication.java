2aadedd977e3f581849e5032d34223fabbdcf3ab
Soumya Raddy <soumya.raddy@tavant.com>
2024-04-04 14:44:55 +0530

    initial config

---
+++ src/main/java/com/example/SchoolManagementSchool/SchoolManagementSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.SchoolManagementSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSchoolApplication.class, args);
+	}
+
+}
